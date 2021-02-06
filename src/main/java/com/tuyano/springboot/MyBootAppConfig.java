8b2c53c87095aa7e8d6036eff135873d25891e76
Yaling <huangyl0723@gmail.com>
2021-02-06 17:04:43 +0900

    Bean

--- src/main/java/com/tuyano/springboot/MyBootAppConfig.java
+++ src/main/java/com/tuyano/springboot/MyBootAppConfig.java
@@ -1,8 +1,13 @@
 package com.tuyano.springboot;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyBootAppConfig {
   
+	@Bean
+	MyDataBean myDataBean() {
+		return new MyDataBean();
+	}
 }
